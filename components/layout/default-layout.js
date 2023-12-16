c68e98f34fcda19e8d2ed0e8cc25aa514a81a64f
Howard <doublebrown850621@gmail.com>
2023-12-16 19:55:08 +0800

    修改首頁對話框樣式

--- components/layout/default-layout.js
+++ components/layout/default-layout.js
@@ -1,11 +1,12 @@
 import Header from './Header'
 import Footer from './footer/footer'
+import style from './default-layout.module.scss'
 
 export default function DefaultLayout({ children }) {
   return (
     <>
       <Header />
-      <main className="bg-primary">{children}</main>
+      <main className={`${style.main} bg-primary`}>{children}</main>
       <Footer />
     </>
   )
--- components/layout/homepage/index.js
+++ components/layout/homepage/index.js
@@ -6,25 +6,23 @@ export default function Homepage() {
   return (
     <>
       <div className={style.main}>
-        <div className="d-flex h-100 align-items-center container">
-          <motion.div
-            className={`${style.dialog} `}
-            initial={{ opacity: 0 }}
-            animate={{ x: 50, opacity: 1 }}
-            transition={{ ease: 'easeOut', duration: 2 }}
-          >
-            <div>
-              <h1>您好！我是陳炫志</h1>
-              <p>
-                歡迎來到我的小站，這裡是我初次嘗試建立的小站
-                <br />
-                建置這個小站的目的是為了更多練習 React.js
-                <br />
-                很高興認識你！
-              </p>
-            </div>
-          </motion.div>
-        </div>
+        <motion.div
+          className={`${style.dialog} `}
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ ease: 'easeOut', duration: 1 }}
+        >
+          <div>
+            <h1>您好！我是陳炫志</h1>
+            <p>
+              歡迎來到我的小站，這裡是我初次嘗試建立的小站
+              <br />
+              建置這個小站的目的是為了更多練習 React.js
+              <br />
+              很高興認識你！
+            </p>
+          </div>
+        </motion.div>
       </div>
     </>
   )
